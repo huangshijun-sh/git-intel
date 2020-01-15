ccc0306d67f5da6ddee7950b1e3b0fb3d856aaa6
tom <69898228@qq.com>
2020-01-15 17:09:55 +0800

    修改后的

--- git-test/src/Main.java
+++ git-test/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git World!");
+
+        System.out.println("Hello Git World!");
+        System.out.println("Hello Git World!");
+        System.out.println("Hello Git World!");
     }
 }
